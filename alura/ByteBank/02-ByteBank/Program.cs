165d6d321e7e3e4cd1f7ee8ec11417a92c03590d
Thone <thonecardoso@gmail.com>
2021-07-06 16:30:36 -0300

    Tipos

---
+++ alura/ByteBank/02-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente conta = new ContaCorrente();
+
+            
+
+            conta.saldo = 100;
+
+            Console.WriteLine("Titular: " + conta.titular);
+            Console.WriteLine("Agencia: " + conta.numeroAgencia);
+            Console.WriteLine("Número: " + conta.numero);
+            Console.WriteLine("Saldo R$" + conta.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ alura/ByteBank/03-ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.numeroAgencia = 123;
+            contaDaGabriela.numero = 12345;
+
+            ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
+            contaDaGabrielaCosta.titular = "Gabriela";
+            contaDaGabrielaCosta.numeroAgencia = 123;
+            contaDaGabrielaCosta.numero = 12345;
+
+            Console.WriteLine(contaDaGabrielaCosta == contaDaGabriela);
+
+            Console.ReadLine();
+
+        }
+    }
+}
