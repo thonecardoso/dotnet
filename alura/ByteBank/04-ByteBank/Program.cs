353c8fc3db9d87a048adc9c5f62fae5e0a401138
Thone <thonecardoso@gmail.com>
2021-07-06 17:24:21 -0300

    Comportamento de Classes

---
+++ alura/ByteBank/04-ByteBank/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+
+            Console.WriteLine(contaDoBruno.saldo);
+            bool saque = contaDoBruno.Sacar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.WriteLine(!saque ? "Saldo Insuficiente" : "Saque Realizado com Sucesso!");
+
+            contaDoBruno.Depositar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+            saque = contaDoBruno.Sacar(500);
+            Console.WriteLine(!saque ? "Saldo Insuficiente" : "Saque Realizado com Sucesso!");
+
+            ContaCorrente contaDaRaquel = new ContaCorrente();
+
+            contaDoBruno.titular = "Raquel";
+
+            Console.WriteLine("Saldo Bruno R$" + contaDoBruno.saldo);
+            Console.WriteLine("Saldo Raquel R$" + contaDaRaquel.saldo);
+            bool tranferido = contaDoBruno.Transferir(50, contaDaRaquel);
+            Console.WriteLine(!tranferido ? "Saldo Insuficiente" : "Tranferência Realizada com Sucesso!");
+            Console.WriteLine("Saldo Bruno R$" + contaDoBruno.saldo);
+            Console.WriteLine("Saldo Raquel R$" + contaDaRaquel.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
