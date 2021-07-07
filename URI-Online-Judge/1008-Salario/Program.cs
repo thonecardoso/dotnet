aae9aae0a7442dbdbf4e47783cfe465a1ba89e3e
Thone <thonecardoso@gmail.com>
2021-07-07 07:46:15 -0300

    1008_Salario

---
+++ URI-Online-Judge/1008-Salario/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1008_Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            int horas = Int32.Parse(Console.ReadLine());
+            double salario = Double.Parse(Console.ReadLine());
+            Console.WriteLine($"NUMBER = {n}");
+            Console.WriteLine($"SALARY = U$ {horas * salario:F2}");
+        }
+    }
+}
