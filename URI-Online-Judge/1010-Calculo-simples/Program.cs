a1dafb4ac089fc34159d87d17fc18b76cab95c4d
Thone <thonecardoso@gmail.com>
2021-07-07 08:36:38 -0300

    1010_Calculo_simples

---
+++ URI-Online-Judge/1010-Calculo-simples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1010_Calculo_simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] l1 = Console.ReadLine().Split(' ');
+            string[] l2 = Console.ReadLine().Split(' ');
+            double resultado = Convert.ToDouble(l1[1]) 
+                * Convert.ToDouble(l1[2]) 
+                + Convert.ToDouble(l2[1]) 
+                * Convert.ToDouble(l2[2]);
+            Console.WriteLine($"VALOR A PAGAR: R$ {resultado:F2}");
+        }
+    }
+}
