dfb8f271d85f67279940f43191f8d55f327f96de
Thone <thonecardoso@gmail.com>
2021-07-07 07:37:49 -0300

    1006_Media_2

---
+++ URI-Online-Judge/1006-Media-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1006_Media_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            double media = (a * 2 + b * 3 + c * 5) / 10.0;
+            Console.WriteLine($"MEDIA = {media:F1}");
+        }
+    }
+}
