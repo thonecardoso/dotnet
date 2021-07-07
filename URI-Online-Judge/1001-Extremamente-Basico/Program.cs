4371e2dc9fbf2c737eef824ce99522c835de9e9c
Thone <thonecardoso@gmail.com>
2021-07-07 06:21:09 -0300

    1001_Extremamente_Basico

---
+++ URI-Online-Judge/1001-Extremamente-Basico/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1001_Extremamente_Basico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"X = {a + b}");
+        }
+    }
+}
