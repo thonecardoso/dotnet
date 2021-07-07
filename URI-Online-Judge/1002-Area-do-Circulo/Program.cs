54a24ed4102b4dc516594440cd41334ccc8f0284
Thone <thonecardoso@gmail.com>
2021-07-07 07:08:19 -0300

    1002_Area_do_Circulo

---
+++ URI-Online-Judge/1002-Area-do-Circulo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1002_Area_do_Circulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio = Convert.ToDouble(Console.ReadLine());
+            double resposta = 3.14159 * raio * raio;
+            Console.WriteLine($"A={resposta:F4}");
+        }
+    }
+}
