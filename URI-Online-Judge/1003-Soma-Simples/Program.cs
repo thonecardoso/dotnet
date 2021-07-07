136b5baf0c821e04071cb9de8119636f807aaa38
Thone <thonecardoso@gmail.com>
2021-07-07 07:33:19 -0300

    1003_Soma_Simples

---
+++ URI-Online-Judge/1003-Soma-Simples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1003_Soma_Simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"SOMA = {a+b}");
+        }
+    }
+}
---
+++ URI-Online-Judge/1005-Media-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1005_Media_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Double.Parse(Console.ReadLine());
+            double b = Double.Parse(Console.ReadLine());
+            Console.WriteLine($"MEDIA = {(a*3.5+b*7.5)/11.0:F5}");
+        }
+    }
+}
