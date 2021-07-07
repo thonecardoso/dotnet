6cd70bc4930b60087cf0c2589910e64c89dcb605
Thone <thonecardoso@gmail.com>
2021-07-07 14:54:35 -0300

    Construtores e membros estáticos

---
+++ alura/ByteBank/07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+        public Cliente Titular { get; set; }
+        private int _agencia;
+        public int Agencia {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _agencia = value;
+                }
+            }
+        }
+        public int Numero { get; set; }
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    _saldo = value;
+                }
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            TotalDeContasCriadas++;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if(this._saldo < valor)
+            {
+                return false;
+            }
+            
+            this._saldo -= valor;
+            return true;
+            
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            
+            this._saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+            
+        }
+
+    }
+}
---
+++ alura/ByteBank/07-ByteBank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867, 12345);
+
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            
+            Console.WriteLine(conta.Numero);
+            Console.WriteLine(conta.Agencia);
+
+            ContaCorrente conta2 = new ContaCorrente(867, 123345);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+        }
+    }
+}
