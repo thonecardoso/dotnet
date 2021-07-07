2127ca8080b36da2273de648bb5b0323350db45b
Thone <thonecardoso@gmail.com>
2021-07-07 13:24:41 -0300

    propriedades aula1

---
+++ alura/ByteBank/06-ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+
+            conta.DefinirSaldo(-12);
+
+
+            Console.WriteLine(conta.ObterSaldo());
+            Console.ReadLine();
+        }
+    }
+}
