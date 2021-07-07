8fe299bbd825e2ae791fc8d2699b3c07fc2a4da2
Thone <thonecardoso@gmail.com>
2021-07-07 07:15:03 -0300

    1004_Produto_Simples

---
+++ URI-Online-Judge/1004-Produto-Simples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1004_Produto_Simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"PROD = {a*b}");
+        }
+    }
+}
