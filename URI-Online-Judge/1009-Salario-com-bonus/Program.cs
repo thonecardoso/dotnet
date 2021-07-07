559131f73a3f8382e1ccc6dd03e0f490fd9a3149
Thone <thonecardoso@gmail.com>
2021-07-07 07:55:49 -0300

    1009_Salario_com_bonus

---
+++ URI-Online-Judge/1009-Salario-com-bonus/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1009_Salario_com_bonus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            double salario = Convert.ToDouble(Console.ReadLine());
+            double vendas = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"TOTAL = R$ {salario + vendas * 0.15:F2}");
+        }
+    }
+}
