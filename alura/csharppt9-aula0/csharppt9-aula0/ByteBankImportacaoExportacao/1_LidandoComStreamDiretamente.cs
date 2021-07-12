22ff423a41f73ae926428f0ff72ff56a462aa1fb
Thone <thonecardoso@gmail.com>
2021-07-12 10:41:42 -0300

    Mais FileStream e leitor StreamReader

---
+++ alura/csharppt9-aula0/csharppt9-aula0/ByteBankImportacaoExportacao/1_LidandoComStreamDiretamente.cs
@@ -0,0 +1,52 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    partial class Program 
+    { 
+        static void LidandoComFileStreamDiretamente()
+        {
+            var enderecoDoArquivo = "contas.txt";
+
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo,FileMode.Open))
+            {
+                var buffer = new byte[1024]; // 1kb
+                var numeroDeBytesLidos = -1;
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    EscreverBuffer(buffer, numeroDeBytesLidos);
+                }
+            }
+
+
+            
+            
+            
+            Console.ReadLine();
+        }
+        
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+            // var utf8 = new UTF8Encoding();
+            // var utf8 = Encoding.Default;
+            var utf8 = Encoding.UTF8;
+
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
+            Console.Write(texto);
+            
+            // foreach (var meuByte in buffer)
+            // {
+            //     Console.Write(meuByte);
+            //     Console.Write(' ');
+            // }
+        }
+    }
+}
--- alura/csharppt9-aula0/csharppt9-aula0/ByteBankImportacaoExportacao/Program.cs
+++ alura/csharppt9-aula0/csharppt9-aula0/ByteBankImportacaoExportacao/Program.cs
@@ -6,47 +6,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ByteBankImportacaoExportacao 
-{ 
-    class Program 
-    { 
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
         static void Main(string[] args)
         {
+            // LidandoComFileStreamDiretamente();
+            
             var enderecoDoArquivo = "contas.txt";
 
-            
-
-            var fluxoDoArquivo = new FileStream(enderecoDoArquivo,FileMode.Open);
-
-            var buffer = new byte[1024]; // 1kb
-            var numeroDeBytesLidos = -1;
-
-            while (numeroDeBytesLidos != 0)
+            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            using (var leitor = new StreamReader(fluxoDeArquivo))
             {
-                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                EscreverBuffer(buffer);
+                while(!leitor.EndOfStream)
+                {
+                    var linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
+                }
             }
-            
 
-            
             Console.ReadLine();
         }
-
-        static void EscreverBuffer(byte[] buffer)
-        {
-            // var utf8 = new UTF8Encoding();
-            // var utf8 = Encoding.Default;
-            var utf8 = Encoding.UTF8;
-
-            var texto = utf8.GetString(buffer);
-            Console.Write(texto);
-            
-            // foreach (var meuByte in buffer)
-            // {
-            //     Console.Write(meuByte);
-            //     Console.Write(' ');
-            // }
-        }
     }
-} 
- 
+}
