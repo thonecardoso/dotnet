1f150e1079a25f8ebc0ce4cd5ace44b58f4145bf
Thone <thonecardoso@gmail.com>
2021-07-12 12:30:53 -0300

    O Flush e arquivos binarios

--- alura/csharppt9-aula0/csharppt9-aula0/ByteBankImportacaoExportacao/3_CriandoArquivo.cs
+++ alura/csharppt9-aula0/csharppt9-aula0/ByteBankImportacaoExportacao/3_CriandoArquivo.cs
@@ -37,6 +37,24 @@ namespace ByteBankImportacaoExportacao
                 escritor.Write("456,65465,456.0,Pedro");
             }
         }
+        
+        static void TestaEscrita() 
+        {
+            var caminhoArquivo = "teste.txt";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoDeArquivo))
+            {
+                for (int i = 0; i < 1000000000; i++)
+                {
+                    escritor.WriteLine($"Linha {i}");
+                    escritor.Flush(); // Despeja o buffer para o Stream
+
+                    Console.WriteLine($"Linha {i} foi escrita no arquivo. Tecle enter p adicionar mais uma!");
+                    Console.ReadLine();
+                }
+            }
+        }
 
     }
 }
