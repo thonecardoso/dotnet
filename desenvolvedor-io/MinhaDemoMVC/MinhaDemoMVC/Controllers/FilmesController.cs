62f20f5228e3b38c7b43c17c15575fe304f651e4
Thone <thonecardoso@gmail.com>
2021-07-14 11:56:37 -0300

    Validações de Formulário

---
+++ desenvolvedor-io/MinhaDemoMVC/MinhaDemoMVC/Controllers/FilmesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaDemoMVC.Models;
+
+namespace MinhaDemoMVC.Controllers
+{
+    public class FilmesController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Adicionar(Filme filme)
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+            return View(filme);
+        }
+    }
+}
