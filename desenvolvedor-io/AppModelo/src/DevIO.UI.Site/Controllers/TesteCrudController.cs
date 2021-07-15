5aa94437a0a7f77f2d601a78e65d5824155a5f48
Thone <thonecardoso@gmail.com>
2021-07-15 10:12:23 -0300

    Configurando o DbContext && Operações CRUD

---
+++ desenvolvedor-io/AppModelo/src/DevIO.UI.Site/Controllers/TesteCrudController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using DevIO.UI.Site.Data;
+using DevIO.UI.Site.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.UI.Site.Controllers
+{
+    public class TesteCrudController : Controller
+    {
+        private readonly MeuDbContext _context;
+
+        public TesteCrudController(MeuDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET
+        public IActionResult Index()
+        {
+            var aluno = new Aluno()
+            {
+                Nome = "Thone",
+                DataDeNascimento = DateTime.Now,
+                Email = "thone@email.com"
+            };
+
+            _context.Alunos.Add(aluno);
+            _context.SaveChanges();
+
+            var aluno2 = _context.Alunos.Find(aluno.Id);
+            var aluno3 = _context.Alunos.FirstOrDefault(a => a.Email == "thone@email.com");
+            var aluno4 = _context.Alunos.Where(a => a.Email == "Thone");
+
+            aluno.Nome = "João";
+            _context.Alunos.Update(aluno);
+            _context.SaveChanges();
+
+            _context.Alunos.Remove(aluno);
+            _context.SaveChanges();
+            
+            return View();
+        }
+    }
+}
--- desenvolvedor-io/AppModelo/src/DevIO.UI.Site/Startup.cs
+++ desenvolvedor-io/AppModelo/src/DevIO.UI.Site/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -18,6 +20,13 @@ namespace DevIO.UI.Site
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<RazorViewEngineOptions>(options =>
@@ -27,6 +36,9 @@ namespace DevIO.UI.Site
                 options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
                 options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
             });
+
+            services.AddDbContext<MeuDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("MeuDbContex")));
             
             services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             
