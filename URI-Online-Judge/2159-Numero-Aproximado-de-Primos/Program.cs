addb6f9cb2b0e2ca0775f32642c6a30eb878ca63
Thone <thonecardoso@gmail.com>
2021-07-15 09:09:53 -0300

    2159_Numero_Aproximado_de_Primos

---
+++ URI-Online-Judge/2159-Numero-Aproximado-de-Primos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2159_Numero_Aproximado_de_Primos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numero = Convert.ToDouble(Console.ReadLine());
+            var x = numero / Math.Log(numero);
+            var y = 1.25506 * numero / Math.Log(numero);
+            Console.WriteLine($"{x:f1} {y:f1}");
+        }
+    }
+}
