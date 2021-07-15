7a9ce40df063a0b431595d0256d6b159336e5c87
Thone <thonecardoso@gmail.com>
2021-07-15 01:21:58 -0300

    Configurando uma injeção de dependência

---
+++ desenvolvedor-io/AppModelo/src/DevIO.UI.Site/Data/PedidoRepository.cs
@@ -0,0 +1,18 @@
+﻿using DevIO.UI.Site.Models;
+
+namespace DevIO.UI.Site.Data
+{
+    public class PedidoRepository : IPedidoRepository
+    {
+        public Pedido ObterPedido()
+        {
+            return new Pedido();
+        }
+        
+    }
+
+    public interface IPedidoRepository
+    {
+        Pedido ObterPedido();
+    }
+}
--- desenvolvedor-io/AppModelo/src/DevIO.UI.Site/Startup.cs
+++ desenvolvedor-io/AppModelo/src/DevIO.UI.Site/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevIO.UI.Site.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -27,6 +28,8 @@ namespace DevIO.UI.Site
             });
             
             services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            
+            services.AddTransient<IPedidoRepository, PedidoRepository>();
             
         }
 
