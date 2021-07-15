34d83ba211d2cff5d56337d7b6c290e1dea41490
Thone <thonecardoso@gmail.com>
2021-07-14 21:24:15 -0300

    1011_Esfera - URI

---
+++ URI-Online-Judge/1011-Esfera/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1011_Esfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var raio = Convert.ToDouble(Console.ReadLine());
+            double volume = 4.0 / 3 * 3.14159 * Math.Pow(raio, 3);
+            Console.WriteLine($"VOLUME = {volume:f3}");
+        }
+    }
+}
