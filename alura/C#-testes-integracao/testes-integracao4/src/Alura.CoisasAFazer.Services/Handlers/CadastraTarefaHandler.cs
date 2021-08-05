d73f5167ffb9173998982ff5858ddffe75975e3f
Thone <thonecardoso@gmail.com>
2021-08-05 16:29:15 -0300

    C# Testes de Integração Alura

---
+++ alura/C#-testes-integracao/testes-integracao4/src/Alura.CoisasAFazer.Services/Handlers/CadastraTarefaHandler.cs
@@ -0,0 +1,43 @@
+﻿using Alura.CoisasAFazer.Core.Models;
+using Alura.CoisasAFazer.Core.Commands;
+using Alura.CoisasAFazer.Infrastructure;
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace Alura.CoisasAFazer.Services.Handlers
+{
+    public class CadastraTarefaHandler
+    {
+        IRepositorioTarefas _repo;
+        ILogger<CadastraTarefaHandler> _logger;
+
+        public CadastraTarefaHandler(IRepositorioTarefas repositorio)
+        {
+            _repo = repositorio;
+            _logger = new LoggerFactory().CreateLogger<CadastraTarefaHandler>();
+        }
+
+        public CommandResult Execute(CadastraTarefa comando)
+        {
+            try
+            {
+                var tarefa = new Tarefa
+                (
+                    id: 0,
+                    titulo: comando.Titulo,
+                    prazo: comando.Prazo,
+                    categoria: comando.Categoria,
+                    concluidaEm: null,
+                    status: StatusTarefa.Criada
+                );
+                _logger.LogDebug("Persistindo a tarefa...");
+                _repo.IncluirTarefas(tarefa);
+                return new CommandResult(true);
+            }
+            catch (Exception)
+            {
+                return new CommandResult(false);
+            }
+        }
+    }
+}
---
+++ alura/C#-testes-integracao/testes-integracao4/tests/Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
@@ -0,0 +1,59 @@
+using Alura.CoisasAFazer.Core.Commands;
+using Alura.CoisasAFazer.Core.Models;
+using Alura.CoisasAFazer.Infrastructure;
+using Alura.CoisasAFazer.Services.Handlers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using Xunit;
+using Moq;
+
+namespace Alura.CoisasAFazer.Testes
+{
+    public class CadastraTarefaHandlerExecute
+    {
+        [Fact]
+        public void DadaTarefaComInfoValidasDeveIncluirNoBD()
+        {
+            //arrange
+            var comando = new CadastraTarefa("Estudar Xunit", new Categoria(100, "Estudo"), new DateTime(2019, 12, 31));
+
+            var options = new DbContextOptionsBuilder<DbTarefasContext>()
+                .UseInMemoryDatabase("DbTarefasContext")
+                .Options;
+            var contexto = new DbTarefasContext(options);
+            var repo = new RepositorioTarefa(contexto);
+
+            var handler = new CadastraTarefaHandler(repo);
+
+            //act
+            handler.Execute(comando); //SUT >> CadastraTarefaHandlerExecute
+
+            //assert
+            var tarefa = repo.ObtemTarefas(t => t.Titulo == "Estudar Xunit").FirstOrDefault();
+            Assert.NotNull(tarefa);
+        }
+
+        [Fact]
+        public void QuandoExceptionForLancadaResultadoIsSuccessDeveSerFalse()
+        {
+            //arrange
+            var comando = new CadastraTarefa("Estudar Xunit", new Categoria("Estudo"), new DateTime(2019, 12, 31));
+
+            var mock = new Mock<IRepositorioTarefas>();
+
+            mock.Setup(r => r.IncluirTarefas(It.IsAny<Tarefa[]>()))
+                .Throws(new Exception("Houve um erro na inclusão de tarefas"));
+
+            var repo = mock.Object;
+
+            var handler = new CadastraTarefaHandler(repo);
+
+            //act
+            CommandResult resultado = handler.Execute(comando);
+
+            //assert
+            Assert.False(resultado.IsSuccess);
+        }
+    }
+}
