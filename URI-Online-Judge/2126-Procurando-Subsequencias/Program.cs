f0fac9a3250e19aa1ff0f7df6259a96974f2d26e
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-09-28 14:37:48 -0300

    2126_Procurando_Subsequencias

---
+++ URI-Online-Judge/2126-Procurando-Subsequencias/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _2126_Procurando_Subsequencias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caso = 0;
+            while (true)
+            {
+                var sub = Console.ReadLine();
+                if (String.IsNullOrEmpty(sub))
+                    break;
+                var str = Console.ReadLine();
+
+                var rx = new Regex(sub);
+                var qt = rx.Matches(str);
+
+                Console.WriteLine($"Caso #{++caso}:");
+                if(qt.Count == 0)
+                {
+                    Console.WriteLine("Nao existe subsequencia\n");
+                }
+                else
+                {
+                    Console.WriteLine($"Qtd.Subsequencias: {qt.Count}");
+                    Console.WriteLine($"Pos: {str.LastIndexOf(sub) + 1}\n");
+                }
+
+            }
+        }
+    }
+}
