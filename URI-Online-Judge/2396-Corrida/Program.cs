1b3436c7a7cf37e030137b065fcbd0fc33ce2ceb
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-09-28 11:02:36 -0300

    2396-Corrida

---
+++ URI-Online-Judge/2396-Corrida/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2396_Corrida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            var n = int.Parse(input[0]);
+            var res = new SortedDictionary<int, int>();
+
+            for(int i = 1; i<=n; i++)
+            {
+                input = Console.ReadLine().Split();
+                res.Add(input.Select(num => int.Parse(num)).Sum(), i);
+            }
+            foreach(var x in res.Take(3))
+            {
+                Console.WriteLine(x.Value);
+            }
+            
+        }
+    }
+}
