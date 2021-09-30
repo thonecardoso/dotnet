4a5c1f8754f024142237ecaa54f457425bcfb185
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-09-30 12:59:31 -0300

    3303_Palavrao

---
+++ URI-Online-Judge/3303-Palavrao/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3303_Palavrao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var p = Console.ReadLine();
+                if (string.IsNullOrEmpty(p))
+                    break;
+                Console.WriteLine(p.Length > 9 ? "palavrao" : "palavrinha");
+            }
+        }
+    }
+}
