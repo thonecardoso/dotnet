b04b6646607e3ffc799c298de5a7aae3447263ae
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-09-30 13:27:15 -0300

    3302_Resposta_Certa

---
+++ URI-Online-Judge/3302-Resposta-Certa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3302_Resposta_Certa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var i = 0;
+            while (true)
+            {
+                var p = Console.ReadLine();
+                if (string.IsNullOrEmpty(p))
+                    break;
+                Console.WriteLine($"resposta {++i}: {p}");
+            }
+        }
+    }
+}
