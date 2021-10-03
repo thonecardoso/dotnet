c800cb273635a14f7358c0711cfb7d9fd40c35ff
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-10-03 11:59:59 -0300

    1800_Onde_Estao_Minhas_Chaves

---
+++ URI-Online-Judge/1800-Onde-Estao-Minhas-Chaves/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1800_Onde_Estao_Minhas_Chaves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var qt = Console.ReadLine().Split().Take(1).Select(v => int.Parse(v)).ToArray().First();
+            var aux = Console.ReadLine().Split().ToList();
+            var dic = new Dictionary<string, int>();
+            aux.ForEach(v => {
+                if (!dic.ContainsKey(v))
+                        dic.Add(v, 0); 
+            });
+            for(int i=0; i<qt; i++)
+            {
+                var x = Console.ReadLine();
+                if (dic.ContainsKey(x))
+                    Console.WriteLine(0);
+                else
+                {
+                    dic.Add(x, 0);
+                    Console.WriteLine(1);
+                }
+                    
+            }   
+        }
+    }
+}
