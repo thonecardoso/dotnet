4e5629b3c3ab660a6935a488d2988bd6fdacce2b
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-10-04 15:23:21 -0300

    3214_Refrigerante

---
+++ URI-Online-Judge/3214-Refrigerante/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _3214_Refrigerante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = Console.ReadLine().Split().Select(v => int.Parse(v)).ToArray();
+            var total = Total(i[0] + i[1], i[2]);
+            Console.WriteLine(total);
+        }
+
+        static int Total(int garrafas, int preco)
+        {
+            if (garrafas < preco) return 0;
+
+            return garrafas / preco + Total(garrafas / preco + garrafas % preco, preco);
+        }
+    }
+}
