769e76f816da62167c64beb03cec7dd45d81fdab
Thone <thonecardoso@gmail.com>
2021-10-16 18:22:36 -0300

    Uri_1607_Avance_as_Letras

---
+++ URI-Online-Judge/1607-Avance-as-Letras/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _1607_Avance_as_Letras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            while (n-- > 0)
+            {
+                var v = Console.ReadLine().Split();
+                var a = v[0];
+                var b = v[1];
+                Console.WriteLine(a.Select((e,i)=>
+                        e<= b[i] 
+                        ?
+                            Math.Abs(e - b[i])
+                        : 
+                            'z' - e  + b[i] - 'a' + 1
+                    ).Sum());
+            }
+        }
+    }
+}
